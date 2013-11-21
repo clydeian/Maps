dbe65f460163f2b642731b1e989bbdd1384c8de4
clydeian <clydeianacaso@hotmail.com>
2013-11-21 02:32:45 -1200

    ok

--- src/com/clydex/maps/MainActivity.java
+++ src/com/clydex/maps/MainActivity.java
@@ -2,77 +2,136 @@ package com.clydex.maps;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements LocationListener {
 
 	private GoogleMap mMap;
+	private Location myLocation;
+	private LatLng latLng;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		 setUpMapIfNeeded();
-		 
-		 // Enabling MyLocation Layer of Google Map
-	        mMap.setMyLocationEnabled(true);
-	        // Get LocationManager object from System Service LOCATION_SERVICE
-	        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		setUpMapIfNeeded();
 
-	        // Create a criteria object to retrieve provider
-	        Criteria criteria = new Criteria();
+		// Enabling MyLocation Layer of Google Map
+		mMap.setMyLocationEnabled(true);
+		// Get LocationManager object from System Service LOCATION_SERVICE
+		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-	        // Get the name of the best provider
-	        String provider = locationManager.getBestProvider(criteria, true);
+		// Create a criteria object to retrieve provider
+		Criteria criteria = new Criteria();
 
-	        // Get Current Location
-	        Location myLocation = locationManager.getLastKnownLocation(provider);
+		// Get the name of the best provider
+		String provider = locationManager.getBestProvider(criteria, true);
 
-	        //set map type
-	        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+		// Get Current Location
+		 myLocation = locationManager.getLastKnownLocation(provider);
 
-	        // Get latitude of the current location
-	        double latitude = myLocation.getLatitude();
+		// set map type
+		mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-	        // Get longitude of the current location
-	        double longitude = myLocation.getLongitude();
+		// Get latitude of the current location
+		double latitude = myLocation.getLatitude();
 
-	        // Create a LatLng object for the current location
-	        LatLng latLng = new LatLng(latitude, longitude);      
+		// Get longitude of the current location
+		double longitude = myLocation.getLongitude();
 
-	        // Show the current location in Google Map        
-	        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+		// Create a LatLng object for the current location
+		latLng = new LatLng(latitude, longitude);
 
-	        // Zoom in the Google Map
-	        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-	        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!"));    
-	        
+		// Show the current location in Google Map
+		mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+		// Zoom in the Google Map
+		mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+		mMap.addMarker(new MarkerOptions()
+				.position(new LatLng(latitude, longitude))
+				.title("You are here!")
+				.icon(BitmapDescriptorFactory
+						.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+
+		// Zoom in the Tyfanny's Map
+		Marker Marker2 = mMap.addMarker(new MarkerOptions().position(
+				new LatLng(8.18706, 126.35402)).title("Tyfanny's Restaurant"));
+		Marker2.getId();
+
+		mMap.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+
+			@Override
+			public void onInfoWindowClick(Marker marker) {
+
+				Toast.makeText(getBaseContext(), "Click Info Window",
+						Toast.LENGTH_SHORT).show();
+			}
+
+		});
+
 	}
-	
-	 @Override
-	    protected void onResume() {
-	        super.onResume();
-	        setUpMapIfNeeded();
-	    }
-	 
-	 private void setUpMapIfNeeded() {
-	        // Do a null check to confirm that we have not already instantiated the map.
-	        if (mMap == null) {
-	            // Try to obtain the map from the SupportMapFragment.
-	            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-	                    .getMap();
-	        }
-	    }
-	 
-	 
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// Get latitude of the current location
+				double latitude = myLocation.getLatitude();
+
+				// Get longitude of the current location
+				double longitude = myLocation.getLongitude();
+
+				// Create a LatLng object for the current location
+				latLng = new LatLng(latitude, longitude);
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
